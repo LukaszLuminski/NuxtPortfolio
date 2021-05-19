59ef154548b883b59a894cee3901bbf63a7bc6c8
Lukasz Luminski <lukasz@bigfootdigital.co.uk>
2021-05-19 09:55:05 +0100

    contact section updated

--- components/Contact.vue
+++ components/Contact.vue
@@ -15,30 +15,32 @@
         <v-divider class="mb-12 contact__divider" />
       </div>
       <v-row class="contact__row mb-8">
-        <v-col>
+        <v-col class="pr-md-10 col-12 col-md-6">
           <v-form ref="form" lazy-validation @submit.prevent="validate">
             <recaptcha />
             <v-row>
               <v-col class="col-12 col-sm-6 pr-sm-5 pb-0">
                 <v-text-field
                   v-model="name"
+                  placeholder="Your Name"
                   class="contact__text-field"
                   :rules="basicRules"
                   type="text"
-                  placeholder="Name"
                   required
-                  outlined
+                  filled
+                  rounded
                   dense
                 />
               </v-col><v-col class="col-12 col-sm-6 pl-sm-0 pt-0 pt-sm-3 pb-0">
                 <v-text-field
                   v-model="email"
+                  placeholder="Your Email"
                   class="contact__text-field"
                   :rules="emailRules"
-                  placeholder="Email"
                   type="text"
                   required
-                  outlined
+                  filled
+                  rounded
                   dense
                 />
               </v-col>
@@ -47,29 +49,33 @@
               <v-col block class="pt-0">
                 <v-text-field
                   v-model="subject"
+                  placeholder="Subject"
                   class="contact__text-field"
                   :rules="basicRules"
-                  placeholder="Subject"
                   type="text"
                   required
-                  outlined
+                  filled
+                  rounded
                   dense
                 />
                 <v-textarea
                   v-model="message"
+                  placeholder="Enter Your Message"
                   auto-grow
                   class="contact__text-field"
-                  outlined
+                  filled
+                  rounded
                   :rules="basicRules"
-                  placeholder="Enter Your Message"
                 />
-                <v-btn large @click="validate">
+                <v-btn dark x-large class="contact__btn" @click="validate">
                   Submit
                 </v-btn>
               </v-col>
             </v-row>
           </v-form>
-        </v-col><v-divider vertical /><v-col><h2>Details</h2></v-col>
+        </v-col><v-divider vertical class="contact__divider__vertical" /><v-col class="pl-md-10 col-12 col-md-5">
+          <h2>Details</h2>
+        </v-col>
       </v-row>
     </v-container>
     <ErrorDialog
@@ -130,16 +136,38 @@ export default {
 
 <style lang="scss">
 .contact {
-    margin-top: -45px;
-    @media (min-width: 768px) {
-   -webkit-clip-path: polygon(0 7%, 100% 0, 100% 100%, 0% 100%);
+  margin-top: -45px;
+  @media (min-width: 768px) {
+    -webkit-clip-path: polygon(0 7%, 100% 0, 100% 100%, 0% 100%);
     clip-path: polygon(0 7%, 100% 0, 100% 100%, 0% 100%);
   }
-    background: linear-gradient(to bottom, #a59b8d, #522c14);
-    -webkit-clip-path: polygon(0 4%, 100% 0, 100% 100%, 0% 100%);
-    clip-path: polygon(0 4%, 100% 0, 100% 100%, 0% 100%);
-    &__divider {
-        background-color: rgba(255, 255, 255, 0.7) !important;
+  background: linear-gradient(to bottom, #a59b8d, #522c14);
+  -webkit-clip-path: polygon(0 4%, 100% 0, 100% 100%, 0% 100%);
+  clip-path: polygon(0 4%, 100% 0, 100% 100%, 0% 100%);
+  &__divider {
+    &__vertical {
+      background-color: rgba(255, 255, 255, 0.4) !important;
     }
+    background-color: rgba(255, 255, 255, 0.7) !important;
+  }
+  &__text-field {
+    .v-input__slot {
+      background-color: white !important;
+      border: none !important;
+    }
+  }
+  &__btn {
+    background-color: #666666 !important;
+  }
 }
+.v-application  .contact__text-field .error--text {
+  color: #ffc966 !important;
+    caret-color: #ffc966 !important;
+}
+.v-text-field fieldset, .v-text-field .v-input__control, .v-text-field .v-input__slot {
+    border-radius: 3px !important;
+}
+// .v-text-field--outlined.v-input--is-focused fieldset, .v-text-field--outlined.v-input--has-state fieldset {
+//       border: 2px solid orange !important;
+// }
 </style>
